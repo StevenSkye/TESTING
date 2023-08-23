d6d137a0b49153c136fb5f1db4d1501f07c9224a
CDani1la <cisla@deakin.edu.au>
2023-08-23 14:26:07 +1000

    Fixed Bike selection on city scene

--- Unity/ProjectSunCycleRedback/Assets/_Scripts/UI/BikeSelection/LoadBike.cs
+++ Unity/ProjectSunCycleRedback/Assets/_Scripts/UI/BikeSelection/LoadBike.cs
@@ -12,8 +12,6 @@ public class LoadBike : MonoBehaviour
         int selectedBike = PlayerPrefs.GetInt("SelectedBike");// get chosen integer
         Debug.Log("selected Character: " + selectedBike);
         GameObject prefab = bikeSelected[selectedBike];//set it to the corresponding bike
-        //GameObject clone = Instantiate(prefab, spawnPoint.position, Quaternion.identity);//clone it (should remove)
-        //use for loop to identify which bike in an array is chosen
         //only hide the ones that arent chosen
         for (int i = 0; i < bikeSelected.Length; i++)
         {
@@ -22,59 +20,5 @@ public class LoadBike : MonoBehaviour
                 bikeSelected[i].SetActive(false);//hide the not chosen bikes
             }
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        /*
-        //----------- ADD COMPONENTS HERE---------------
-        //clone.AddComponent<ScriptName>();
-
-        //---------- add rigidbody ---------------
-        Rigidbody rb = clone.AddComponent<Rigidbody>();
-        rb.mass = 100f;
-        //rb.useGravity = true;
-        rb.angularDrag = 0.05f;
-        rb.collisionDetectionMode = CollisionDetectionMode.Discrete;
-        /*
-        //---------- add hover script -------------
-        Hover HoverComp = clone.AddComponent<Hover>();
-        HoverComp.minimumHeight = 192f;
-        HoverComp.hoverHeight = 1f;
-        HoverComp.hoverForce = 20000f;
-        HoverComp.hoverDamp = 0.5f;
-        
-
-        //---------- add Bike Controls script ---------------
-        BikeControls bikeControlsComp = clone.AddComponent<BikeControls>();
-        bikeControlsComp.speed = 10f;
-        bikeControlsComp.turnSpeed = 5f;
-
-
-
-
-        //add mission controller
-        clone.AddComponent<MissionController>();
-        */
     }
 }
